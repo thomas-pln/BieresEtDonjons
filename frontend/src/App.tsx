f9263acaf4458570f12c489251e71f1c0201fa29
Thomas/ <thomas.pln@protonmail.com>
2024-06-04 00:10:33 +0200

    home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,16 @@
 // App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/account/LoginPage.tsx';
 
 const App: React.FC = () => {
     return (
         <Router>
             <Layout>
-                <Switch>
-                    <Route path="/" exact component={HomePage} />
-                    <Route path="/login" component={LoginPage} />
-                </Switch>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                </Routes>
             </Layout>
         </Router>
     );
